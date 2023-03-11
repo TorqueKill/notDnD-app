258d6acf53fb3e0a1ef28856647e81e40c244000
Abdul Muiz <113927276+TorqueKill@users.noreply.github.com>
2023-03-11 22:21:21 +0500

    Update daddyStore.js

--- src/daddyStore.js
+++ src/daddyStore.js
@@ -8,15 +8,23 @@ var peer = new Peer({ initiator: isMaster,
     config: {
         iceServers: [
             {
-                url: 'turn:relay.backups.cz',
-                credential: 'webrtc',
-                username: 'webrtc'
-            },
-            {
-                url: 'turn:relay.backups.cz?transport=tcp',
-                credential: 'webrtc',
-                username: 'webrtc'
-            },
+                url: "stun:global.stun.twilio.com:3478"
+              },
+              {
+                username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+                credential: "tE2DajzSJwnsSbc123",
+                url: "turn:global.turn.twilio.com:3478?transport=udp"
+              },
+              {
+                username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+                credential: "tE2DajzSJwnsSbc123",
+                url: "turn:global.turn.twilio.com:3478?transport=tcp"
+              },
+              {
+                username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+                credential: "tE2DajzSJwnsSbc123",
+                urls: "turn:global.turn.twilio.com:443?transport=tcp"
+              }
             /*
             {
                 url: 'turn:openrelay.metered.ca:80',
